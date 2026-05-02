5a1a3620b317d6be47f2cc618c47ba14d5f89add
im-vishu <vishant.22btcse0138@dbuumail.in>
2026-05-02 23:05:35 +0530

    Prisma Client successfully executing queries

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,6 @@
 const { PrismaClient } = require("@prisma/client");
-const { PrismaPg } = require("@prisma/adapter-pg");
-const env = require("./env");
-
-const adapter = new PrismaPg({
-  connectionString: env.DATABASE_URL
-});
 
 const prisma = new PrismaClient({
-  adapter,
   log:
     process.env.NODE_ENV === "development"
       ? ["query", "info", "warn", "error"]
